21b321be414e0975ea6b0fb75f3e1354de55a42a
Enrique Navarro <enrique.navarro64@gmail.com>
2019-03-20 21:42:17 -0600

    Scanner

--- Hashtable.java
+++ Hashtable.java
@@ -1,3 +1,8 @@
+/*
+System.out.println("Dame velocidad: ");
+Scanner lector = new Scanner(System.in);
+double velocidad = lector.nextDouble();
+*/
 public class Hashtable<E> {
 public static final int M =23; //Tamaño del listado
 List<E>[] data = new List[M]; //Arreglo de Listas
